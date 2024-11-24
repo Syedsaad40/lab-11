46966ac84592590495bb3407edb1bd59ffdd379b
Syedsaad40 <mosyedsaad11@gmail.com>
2024-11-24 16:23:39 +0500

    Update q3.c

--- lab-11/q3.c
+++ lab-11/q3.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-	
-	
-	
-	
+#include<string.h>
+int main(void){
+	FILE *file;
+	FILE *copy;
+	file=fopen("file.txt","r");
+	copy=fopen("copy.txt","w");
+	if(file==NULL||copy==NULL){
+		printf("Error in opening file\n");
+		return 1;
+	}
+	char c;
+	while((c=fgetc(file))!= EOF){
+	        fputc(islower(c) ? toupper(c) : c, copy);
+	}
+	fclose(file);
+	fclose(copy);
 }
